53c176a671c102765a9c6294b16631e4c08f70c1
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2025-08-19 21:37:07 +0300

    Fix: Hardcode server's static root.

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATIC_ROOT = "/var/www/ptua18-library-deployment/staticfiles"
+STATIC_ROOT = "/home/ptua18_user/ptua18-library-deployment/staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
